648d11a64a9cbb9f4ddb499492fece6b8853c236
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2026-03-15 18:48:59 -0700

    Update avenLib.js

--- avenLib.js
+++ avenLib.js
@@ -2,13 +2,15 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: eye-slash;
 //backend utils to make life easier
-const VERSION="1.0.0"
+const VERSION="1.0.1"
 function shuffle(l){
-  let out=[...l]
-  for (let i=1;i<out.length;i++) {
-    let j = (0|Math.random()*(i+1))
-    [out[i],out[j]]=[out[j],out[i]]
+  let out = [...l]
+  for (let i = 1; i < out.length; i++) {
+    let j = (0 | Math.random() * (i + 1))
+    let t = out[i]
+    out[i] = out[j]
+    out[j] = t
   }
   return out
 }
-module.exports.shuffle=(l)=>shuffle(l)
+module.exports.shuffle=((l)=>shuffle(l))
