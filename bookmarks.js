e70ab27f38a5518e528c252346262d87d65e5b6e
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2026-03-20 07:54:41 -0700

    bookmarks is real now!

---
+++ bookmarks.js
@@ -0,0 +1,12 @@
+/*
+Second try, I forgot to commit the first one.
+This is a guide for what to do after installing my scripts:
+1. Go into settings in Scriptable
+2. Scroll down to "Bookmarks"
+3. Add the following ([Bookmark Name] - [Script Filename]):
+    avenLib - avenLib.js
+    eddaLib - eddaLib.js
+    FBM - flagBackgroundModule.js
+Good job!
+This will be updated for other libraries when I feel like it.
+*/
