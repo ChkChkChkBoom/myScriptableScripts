cc7660406d44f430952565217c1f677f68ed760b
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2026-03-24 06:42:48 -0700

    Update secrets.js
    It should work

--- secrets.js
+++ secrets.js
@@ -1,12 +1,13 @@
-const VERSION = "1.0.0"
+const VERSION = "1.1.0"
 let n=FileManager.iCloud()
 var eddaLib=importModule(n.bookmarkedPath("eddaLib"))
+let dir= n.documentsDirectory()
 async function loadNames(){
-    let path = fm.joinPath(dir, "names.txt")
-    if (!fm.isFileDownloaded(path)) {
-        await fm.downloadFileFromiCloud(path)
+    let path = n.joinPath(dir, "names.txt")
+    if (!n.isFileDownloaded(path)) {
+        await n.downloadFileFromiCloud(path)
     }
-    let text = fm.readString(path)
+    let text = n.readString(path)
     let list = []
     for (let line of text.split("\n")) {
         line = line.trim()
