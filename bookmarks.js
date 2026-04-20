b52670455809acc5fbed660a997a72e0cad46c98
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2026-04-20 06:33:09 -0700

    Update bookmarks.js
    Clipboard update

--- bookmarks.js
+++ bookmarks.js
@@ -2,16 +2,18 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: green; icon-glyph: book;
 /*
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 This is a guide for what to do after installing my scripts:
 1. Go into settings in Scriptable
 2. Scroll down to "Bookmarks"
 3. Add the following ([Bookmark Name] - [Script Filename]):
+    absorbLib - absorbLib.js
+    adaptationLib - adaptationLib.js
     avenLib - avenLib.js
     eddaLib - eddaLib.js
     lucaLib - lucaLib.js
-    adaptationLib - adaptationLib.js
     FBM - flagBackgroundModule.js
+    clipboards - any clipboard folder in the scriptable iCloud
 4. Run secrets.js (handles the Keychain system) and follow the instructions there.
 Good job!
 This will be updated for other libraries when I feel like it.
