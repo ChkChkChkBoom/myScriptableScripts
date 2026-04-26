62ea39f382d9d7175bba09db98d629cccfc8019d
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2026-04-26 15:46:19 -0700

    Update init.js
    Forgot the version

--- init.js
+++ init.js
@@ -2,6 +2,7 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: green; icon-glyph: magic;
 //does all the setup a script can do.
+const VERSION="1.0.0"
 //makes a clipboard folder
 let fm=FileManager.iCloud()
 if (!fm.fileExists(fm.joinPath(fm.documentsDirectory(), "clipboards"))){
