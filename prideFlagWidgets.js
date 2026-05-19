51f1b4ddeb3ba5141b05e7c4864dc730ec082d0d
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2026-05-18 21:08:40 -0700

    Update prideFlagWidgets.js
    Uses flagStorage

--- prideFlagWidgets.js
+++ prideFlagWidgets.js
@@ -1,8 +1,5 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: light-blue; icon-glyph: flag-checkered;
 //What this needs: flagBackgroundModule.js bookmarked as FBM (might put into another lib), avenLib.js bookmarked as avenLib
-const VERSION="1.3.3"
+const VERSION="1.4.0"
 const handler=FileManager.iCloud()
 const flagMaker=importModule(handler.bookmarkedPath("FBM"))
 const avenLib=importModule(handler.bookmarkedPath("avenLib"))
@@ -11,41 +8,7 @@ let testFlag="asexual"
 let mainDebug=false||masterDebug
 let widgetDebug=false||masterDebug
 let mode=(args.widgetParameter || (Keychain.get("baseFlag") || "asexual")).toLowerCase()
-var g={
-  "asexual":[["black gray white purple",false]],
-  "pride":[["red orange yellow green blue purple",false]],
-  "aroace":[["orange yellow white lightBlue blue",false]],
-  "transgender":[["lightBlue pink white pink lightBlue",false]],
-  "nonbinary":[["yellow white purple black",false]],
-  "lesbian":[["red orange yellow white pink magenta purple",false]],
-  "aromantic":[["darkGreen green white gray black",false]],
-  "pansexual":[["magenta yellow blue",false]],
-  "demigirl":[["darkGray lightGray pink white"+" "+"darkGray lightGray pink".split(" ").reverse().join(" "),false]],
-  "demigender":[["darkGray lightGray yellow white"+" "+"darkGray lightGray yellow".split(" ").reverse().join(" "),false]],
-  "demiboy":[["darkGray lightGray blue white blue lightGray darkGray",false]],
-  "transfem":[["white pink magenta black",false]],
-  "transmasc":[["white lightBlue blue black",false]],
-  "quoiromantic":[["black green blue gray",false]],
-  "quoisexual":[["black white green blue",false]],
-  "femaric":[["black white pink",true]],
-  "mascic":[["black white blue",true]],
-  "cupioquoiromantic":[["black green white pink magenta",false]],
-  "orientedaroace":[["black gray white cyan",false],["darkGreen green white cyan blue",false]],
-  "bisexual":[["magenta magenta magenta magenta purple purple purple blue blue blue blue",false]],
-  "androgyne":[["magenta darkPurple blue",true],["gray gray gray purple purple purple purple gray pink pink pink pink gray gray gray",false]]
-}
-avenLib.readFile(handler.joinPath(handler.documentsDirectory(),"flagNames.txt"),"\n").forEach(x=>{
-  if (x[0]==="#"){
-    //no
-  }else{
-    let s=x.split("-")
-    let name=s[0]
-    let aliases=s[1].split(",")
-    for (let alias of aliases){
-      g[alias]=g[name]
-    }
-  }
-})
+var g=flagStorage.flagDict
 g["random"]=g[avenLib.shuffle(Object.keys(g))[0]]
 function main(flag,subset=0){
   let sub
